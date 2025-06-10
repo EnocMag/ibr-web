1f182425e808b4e91bff06bd28f984aa302cf144
Enoc Magallón <magallon.estrada.e@gmail.com>
2025-06-10 08:59:51 -0600

    add astro.yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://enocmag.github.io/ibr-web',
-    base: '/ibr-web'
+    site: 'https://EnocMag.github.io',
+    base: 'ibr-web'
 });
