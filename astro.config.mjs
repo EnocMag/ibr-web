9702c3349ce2771706b15295e520f233268d5ba8
Enoc Magallón <magallon.estrada.e@gmail.com>
2025-06-09 21:18:46 -0600

    Configure site and base for deployment on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://enocmag.github.io/ibr-web',
+    base: '/ibr-web'
+});
